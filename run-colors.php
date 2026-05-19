cad83ba563d9e0ffdd0e8caa80348c961a5e36ca
Nux <egil@wp.pl>
2026-05-19 21:01:47 +0200

    color fix?

--- run-colors.php
+++ run-colors.php
@@ -1,11 +1,13 @@
 <?php
+$green = chr(27) . '[32m';
+$reset = chr(27) . '[0m';
+
 $domains = array();
 for ($i = 1; $i <= 30; $i++) {
-    $domains[] = sprintf('molapp-%04d', $i);
+	$domains[] = sprintf('molapp-%04d', $i);
 }
 
 foreach ($domains as $domain) {
-    // echo "\nChecked: [$domain].";
-    echo "\n\033[32mChecked\033[0m: [$domain].";
-    sleep(1);
+	echo "\n{$green}Checked{$reset}: [$domain].";
+	sleep(1);
 }
